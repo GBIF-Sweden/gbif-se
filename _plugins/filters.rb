c91315d6c4db46327557dcc82165e4177503be17
Mats Bovin <mats.bovin@gmail.com>
2023-05-16 12:59:40 +0200

    Sorting and cleanup

--- _plugins/filters.rb
+++ _plugins/filters.rb
@@ -1,23 +1,24 @@
 module Jekyll
     module EventFilters
 
-        def coming_events(posts)
-            posts.select do |post|
-                end_date = post.data['to'] ? post.data['to'] : post.data['from']
+        def coming_events(events)
+            events.select do |event|
+                end_date = event.data['to'] ? event.data['to'] : event.data['from']
                 if end_date.to_s[0..9] >= DateTime.now.strftime("%Y-%m-%d")
-                    post
+                    event
                 end
             end
         end
 
-        def past_events(posts)
-            posts.select do |post|
-                end_date = post.data['to'] ? post.data['to'] : post.data['from']
+        def past_events(events)
+            events.select do |event|
+                end_date = event.data['to'] ? event.data['to'] : event.data['from']
                 if end_date.to_s[0..9] < DateTime.now.strftime("%Y-%m-%d")
-                    post
+                    event
                 end
             end
         end
+
     end
 end
 
