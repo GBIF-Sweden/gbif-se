09ce7a67633d8881597806ba4e0516a0311c09ca
Mats Bovin <mats.bovin@gmail.com>
2024-07-13 18:02:25 +0200

    Fetch record count for checklist. Format record count

--- script/fetch-datasets.sh
+++ script/fetch-datasets.sh
@@ -5,10 +5,8 @@ set -e
 
 echo "Fetching datasets from gbif.org..."
 
-# Fetch datasets
+# Fetch and decorate datasets
 curl -sS --max-time 30 --retry 1 "https://api.gbif.org/v1/dataset/search?publishingCountry=SE&limit=500" --output _data/datasets-se.json
-
-# Fetch record count if missing
 node ./script/decorate-datasets.js
 
 echo "Done"
