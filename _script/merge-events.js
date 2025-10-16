ff8c2b5f3d1b891fb7da518fa5363977da37ef99
Mats Bovin <mats.bovin@gmail.com>
2025-10-16 13:53:45 +0200

    Remove events older than two years

--- _script/merge-events.js
+++ _script/merge-events.js
@@ -1,6 +1,7 @@
 // This script adds and updates events from the gbif.org calendar feed (gbif-org-events.json)
 // to the repo file (all-events.json). The purpose is to be able to keep past events as the
 // gbif.org calendar feed only contain upcoming events.
+// Events are removed when they are older than two years.
 
 const fs = require("fs");
 const util = require("util");
@@ -17,6 +18,7 @@ const getExistingEvent = (uid) => {
 const gbifCurrentEvents = JSON.parse(fs.readFileSync("./_data/gbif-org-events.json", "utf8"));
 const allEvents = JSON.parse(fs.readFileSync("./_data/all-events.json", "utf8"));
 
+// Loop through gbif.org upcoming events and add or update events
 gbifCurrentEvents.VCALENDAR[0].VEVENT.forEach(event => {
   const existingEvent = getExistingEvent(event.uid);
   if (existingEvent === undefined) {
@@ -28,4 +30,10 @@ gbifCurrentEvents.VCALENDAR[0].VEVENT.forEach(event => {
   }
 });
 
-fs.writeFileSync('./_data/all-events.json', JSON.stringify(allEvents, null, 2))
+// Remove events older than two years
+const now = new Date();
+const lastValidDate = new Date(now.getFullYear() - 2, now.getMonth(), now.getDate());
+const result = allEvents.filter(event => event.end_time >= lastValidDate.toISOString());
+
+// Write the updated events back to all-events.json
+fs.writeFileSync('./_data/all-events.json', JSON.stringify(result, null, 2))
